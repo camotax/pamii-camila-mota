319a6caaa1520f352bdbd5ee69881c140b3523f5
camotax <camilafemota12@gmail.com>
2023-03-08 08:39:44 -0300

    Aula sobre Javascript com variáveis e funções

---
+++ aula 02/funcoesevariaveis.js
@@ -0,0 +1,18 @@
+// utilize o nome correto para variável, para manutenção
+// se acostume a colocar nome de variáveis em inglês, é um padrão
+var variable1 = "olá" //forma antiga mas válida
+let variable2 = 3 //mais usado, diferente se está dentro ou fora da função
+const variable3 = 67
+
+console.log(variable1)
+console.log(typeof variable2)
+
+const obj = {
+    nome: "Camila",
+    idade: 23
+}
+
+//JSON - Javascript Notation Object
+console.log(obj)
+console.log(obj.nome) // assim se mostra o nome, há sub variáveis
+console.log(typeof obj)
